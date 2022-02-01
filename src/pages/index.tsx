deb634f535d6881fcb830972fdeb0ff7fa0890ba
Suraj Thapa <stsurajst@gmail.com>
2022-02-01 12:37:06 +0545

    user signup and signincomplete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import { Typography } from "@mui/material";
+import { useUser } from "../context/AuthContext";
 export default function Home() {
+  const { user } = useUser();
+
+  console.log("USER::", user);
   return <Typography variant="h1">Hello world</Typography>;
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,21 +3,18 @@ import { Auth } from "aws-amplify";
 import { Alert, Button, Grid, Snackbar, TextField } from "@mui/material";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useUser } from "../context/AuthContext";
-import { CognitoUser } from "@aws-amplify/auth";
 import { useRouter } from "next/router";
 interface IFormInput {
   Username: string;
-  Email: string;
+
   Password: string;
-  Code: string;
 }
 
-export default function Signup() {
+export default function Login() {
   const { user, setUser } = useUser();
   const router = useRouter();
   const [open, setOpen] = useState(false);
-  const [showCode, setShowCode] = useState<boolean>(false);
-  const [signUpError, setSignUpError] = useState<string>("");
+  const [loginError, setLoginError] = useState<string>("");
   const {
     handleSubmit,
     formState: { errors },
@@ -28,17 +25,22 @@ export default function Signup() {
     console.log("data::", data);
 
     try {
-      if (showCode) {
-        confirmSignUp(data);
-      } else {
-        signUpWithEmailAndPassword(data);
-        setShowCode(true);
-      }
+      loginUser(data);
     } catch (err) {
-      setSignUpError(err.message);
+      setLoginError(err.message);
       setOpen(true);
     }
   };
+
+  async function loginUser(data: IFormInput) {
+    const { Username, Password } = data;
+    try {
+      await Auth.signIn(Username, Password);
+      router.push("/");
+    } catch (err) {
+      throw err;
+    }
+  }
 
   const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
     if (reason === "clickaway") {
@@ -47,36 +49,6 @@ export default function Signup() {
 
     setOpen(false);
   };
-
-  async function signUpWithEmailAndPassword(data: IFormInput): Promise<CognitoUser> {
-    const { Username, Password, Email } = data;
-    try {
-      const { user } = await Auth.signUp({
-        username: Username,
-        password: Password,
-        attributes: {
-          email: Email,
-          name: Username,
-        },
-      });
-      console.log("amp user>", user);
-      return user;
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async function confirmSignUp(data: IFormInput) {
-    const { Username, Code } = data;
-    try {
-      await Auth.confirmSignUp(Username, Code);
-      router.push("/login");
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  console.log("user>>", user);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -109,19 +81,7 @@ export default function Signup() {
             })}
           />
         </Grid>
-        <Grid item>
-          <TextField
-            variant="outlined"
-            id="email"
-            label="Email"
-            type="email"
-            error={errors?.Email?.message ? true : false}
-            helperText={errors?.Email?.message ? errors.Email.message : null}
-            {...register("Email", {
-              required: { value: true, message: "Please enter a valid email" },
-            })}
-          />
-        </Grid>
+
         <Grid item>
           <TextField
             variant="outlined"
@@ -139,40 +99,16 @@ export default function Signup() {
             })}
           />
         </Grid>
-        {showCode && (
-          <Grid item>
-            <TextField
-              variant="outlined"
-              id="code"
-              label="Code"
-              type="text"
-              error={errors?.Code?.message ? true : false}
-              helperText={errors?.Code?.message ? errors.Code.message : null}
-              {...register("Code", {
-                required: { value: true, message: "Please enter a Code" },
-                minLength: {
-                  value: 6,
-                  message: "Enter a valid Code",
-                },
-                maxLength: {
-                  value: 6,
-                  message: "Enter a valid Code",
-                },
-              })}
-            />
-          </Grid>
-        )}
-
         <Grid item>
           <Button variant="contained" type="submit">
-            {showCode ? "Confirm User" : "Sign Up"}
+            Login
           </Button>
         </Grid>
       </Grid>
 
       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
-          {signUpError}
+          {loginError}
         </Alert>
       </Snackbar>
     </form>
